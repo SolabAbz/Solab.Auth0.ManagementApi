9616b7b5b4dcb57b0ed19a86fc9c851ffe985a93
Max Vasilyev <trailmax1@gmail.com>
2018-05-02 00:32:02 +0100

    Start of test project

---
+++ src/Tests/Auth0AuthenticationApiClientTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Solab.Auth0.ManagementApi;
+using System;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Settings
+    {
+        public String TestGroupId { get; set; }
+        public String ApiUrl { get; set; }
+        public String Auth0Domain { get; set; }
+        public String Auth0ClientId { get; set; }
+        public String Auth0ClientSecret { get; set; }
+    }
+
+    [TestFixture]
+    public class Auth0AuthenticationApiClientTests
+    {
+        Auth0AuthenticationApiClient sut;
+
+        [SetUp]
+        public async Task SetUpAsync()
+        {
+            //sut = new Auth0AuthenticationApiClient(apiUrl, token);
+        }
+
+    }
+}
